49e2f8d8229e7494a937b3338fe24cc4e580e36b
Zuhair Parvez <zuhairparvez@gmail.com>
2016-11-13 01:50:10 -0600

    Fix comments and colors in VRMountainScene

--- scenes/VRMountainScene.js
+++ scenes/VRMountainScene.js
@@ -7,19 +7,23 @@ import Flashcard from '../components/Flashcard';
 
 /*setting up the entire mountain scene experience*/
 class VRMountainScene extends React.Component {
-    /*creates instance of object*/
+  
   constructor(props){
+    // creates instance of object
     super(props);
     this.state = {color: 'red'};
   }
-/*visually creates the scene*/
+ 
+  
+    
   render () {
+  // visually creates the scene
+        // <!-- can't seem to get a color for this because it's a primitive & not an entity -->
     return (
       <Scene>
         <Flashcard position={[0,1,-2]} text={"Hello, World!"} scale={0.5} color={this.state.color}/>
         <Entity mountain={{color: '#A0522D'}}/>
-        /*can't seem to get a color for this because it's a primitive & not an entity*/
-        <Entity primitive='a-sky' onClick={() => { console.log('Clicked!'); }}/>
+        <Entity primitive='a-sky' material={{color: 'blue'}} onClick={() => { console.log('Clicked!'); }}/>
       </Scene>
     );
   }
