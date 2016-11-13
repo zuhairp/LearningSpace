505d04171cadc19430726896c3af5ae6937d2c25
Zuhair Parvez <zuhairparvez@gmail.com>
2016-11-12 18:07:18 -0600

    Refactor to use aframe-react

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-let aframe = require('aframe')
+import 'aframe';
+import {Entity, Scene} from  'aframe-react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class VRScene extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {color: 'red'};
+  }
+
+  changeColor() {
+   const colors = ['red', 'orange', 'yellow', 'green', 'blue'];
+    this.setState({
+      color: colors[Math.floor(Math.random() * colors.length)]
+    });  
+  }
+
+  render () {
+    return (
+      <Scene>
+        <Entity geometry={{primitive: 'box'}} material="color: red" position={[0, 1, -1]}/>
+      </Scene>
+    );
+  }
+  
+}
+
+ReactDOM.render(<VRScene />, document.querySelector('.scene-container'));
--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,16 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel',
         query: {
-          presets: ['es2015']
+          presets: ['es2015', 'react']
         }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
       }
     ]
   }
